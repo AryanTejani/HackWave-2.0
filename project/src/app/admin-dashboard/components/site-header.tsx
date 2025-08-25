b03db3bdf7a39a5bc93c9a9bea413c402be91993
harendra <jeaturajabundela@gmail.com>
2025-08-25 06:32:12 +0530

    company detail form

--- project/src/app/admin-dashboard/components/site-header.tsx
+++ project/src/app/admin-dashboard/components/site-header.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 
 export function SiteHeader() {
@@ -5,7 +7,13 @@ export function SiteHeader() {
     <header className="bg-background/90 sticky top-0 z-10 flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
         <h1 className="text-base font-medium">Shipment Management</h1>
-     
+     <div>
+      <Link href="/admin-dashboard/upload-data">
+      <Button>
+        Upload data
+        </Button>
+      </Link>
+     </div>
       </div>
     </header>
   );
